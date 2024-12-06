10ae5ec9c4c8556901750e38c5da7febedb2f009
williammadie <william.madie0@gmail.com>
2024-12-06 06:04:59 +0100

    expose headers for cors

--- podcast_storage/main.py
+++ podcast_storage/main.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -17,6 +15,12 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=[
+        'Content-Type',
+        'Authorization',
+        'cache-control',
+        'expires'
+        ]
 )
 
 
